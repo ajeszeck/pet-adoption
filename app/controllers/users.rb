c32b6a81aae6ce7d66d227bfcc85a2eea447c0d4
Anna Jeszeck <email": null>
2017-10-12 16:26:58 -0700

    Change access to certain links based on if there is a logged in user, add login and logout.

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -1,3 +1,16 @@
 get '/users/new' do
   erb :'/users/new'
 end
+
+
+post '/users' do
+  user = User.new(params[:user])
+  user.password = params[:password]
+  if user.save
+    session[:user_id] = user.id
+    redirect '/'
+  else
+    @errors = user.errors.full_messages
+    erb :'/users/new'
+  end
+end
