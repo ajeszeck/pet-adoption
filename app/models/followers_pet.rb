fbde054799b280fa4d1fe8fec9aae4c5245a1e11
Anna Jeszeck <email": null>
2017-10-13 10:20:02 -0700

    Add validation to followers_pet model to prevent user from following the same pet twice.

--- app/models/followers_pet.rb
+++ app/models/followers_pet.rb
@@ -1,4 +1,5 @@
 class FollowersPet < ActiveRecord::Base
   belongs_to :user, foreign_key: 'follower_id'
   belongs_to :pet
+  validates_uniqueness_of :pet_id, scope: :follower_id, message: "is already on your follow list."
 end
