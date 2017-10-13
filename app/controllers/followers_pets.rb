8c2667fe8149b4cd1388c8f44854ba2e8dd18cf5
Anna Jeszeck <email": null>
2017-10-13 10:44:34 -0700

    Fine tune pet follows relationships and validation error display.

--- app/controllers/followers_pets.rb
+++ app/controllers/followers_pets.rb
@@ -1,13 +1,12 @@
 post '/pets/:id/followers_pets' do
   @pet = Pet.find(params[:id])
-  follow = FollowersPet.new(follower_id: User.find(session[:user_id]), pet_id: params[:id])
+  follow = FollowersPet.new(follower_id: User.find(session[:user_id]).id, pet_id:
+   params[:id])
   # @pet.followers << User.find(session[:user_id])
   if follow.save
     redirect "/pets/#{@pet.id}"
   else
-    p "*" * 200
     @errors = follow.errors.full_messages
-    p @errors
     erb :'/pets/show'
   end
 end
